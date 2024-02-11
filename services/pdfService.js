83430a39b38c420974c5041d5f30fd2859a69033
shivamks2712 <shivamks2712@gmail.com>
2024-02-11 18:48:19 +0530

    fix

--- services/pdfService.js
+++ services/pdfService.js
@@ -4943,7 +4943,7 @@ module.exports = {
 
 <body>
     <div class="main">
-        <div style="position: relative;height:1550px">
+        <div style="position: relative;height:1500px">
             <div class="main_first">
                 <div class="main_first_img">
                 <img src="https://i.ibb.co/WxYF57b/logo-1.png" alt="" style="height: 125px;width: 125px;">
