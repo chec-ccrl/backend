9a809b7ec7cbfee687f86c0111426c97b29927cb
shivamks2712 <shivamks2712@gmail.com>
2024-03-09 00:12:05 +0530

    .

--- services/pdfService.js
+++ services/pdfService.js
@@ -7657,7 +7657,7 @@ module.exports = {
                         <div class="main_second_select1_label label">
                             RENT :
                         </div>
-                        <div class="main_second_select1_check" style="margin-left: 48px;">
+                        <div class="main_second_select1_check" style="margin-left: 78px;">
                            ${rentSourceCheckBox1}
                             ${rentSourceCheckBox2}
                         </div>
