f91eaeffb9279c8d6191c5334cfb14a8e195d2f3
shivamks2712 <shivamks2712@gmail.com>
2024-03-16 15:38:14 +0530

    dc

--- services/pdfService.js
+++ services/pdfService.js
@@ -1720,7 +1720,7 @@ module.exports = {
                 <div class="main_page3_first_one_chart">
                   <canvas
                     id="main_page3_first_one_chart_main"
-                    style="height: 300px; width: 160px"
+                    style="height: 300px; width: 155px"
                   ></canvas>
                 </div>
               </div>
