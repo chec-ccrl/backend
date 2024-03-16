b45da3154ae75921b01bdb71e151856a2bf11ffa
shivamks2712 <shivamks2712@gmail.com>
2024-03-16 13:14:31 +0530

    fifi

--- services/pdfService.js
+++ services/pdfService.js
@@ -1551,9 +1551,9 @@ module.exports = {
                   <select
                     name="cars"
                     id="cars"
-                    style="width: 200px; margin-left: 30px;margin-right:27px;font-size:12px;font-weight:400;"
+                    style="width: 200px; margin-left: 30px;margin-right:27px;font-size:12px;font-weight:400;height:50px"
                   >
-                    <option value="volvo" style="height:30px">${affordability}</option>
+                    <option value="volvo">${affordability}</option>
                   </select>
                 </div>
                 <div class="main_second_select1" style="margin-left:30px;">
