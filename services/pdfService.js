5e3b6c689c6a7e72dc8e07afc02f47c0b4a19788
shivamks2712 <shivamks2712@gmail.com>
2024-03-24 22:26:53 +0530

    .

--- services/pdfService.js
+++ services/pdfService.js
@@ -1756,7 +1756,7 @@ module.exports = {
               <div class="main_page3_first_one_break"></div>
               <div class="main_page3_second_two">
                 <div class="flex align-center justify-between">
-                  <div class="main_page3_first_one_txt" style="width: 75%;font-size:22px">
+                  <div class="main_page3_first_one_txt" style="width: 75%;font-size:21px">
                     AVERAGE OPTIMAL INCOME BEFORE TAX
                   </div>
                   <div class="main_page3_first_one_main_head_sub_two">$${numberWithCommas(
