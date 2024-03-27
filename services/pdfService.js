925cafcc99fe1c17cb146afdb99f1efe140fdc1e
shivamks2712 <shivamks2712@gmail.com>
2024-03-28 02:39:18 +0530

    fr

--- services/pdfService.js
+++ services/pdfService.js
@@ -3051,11 +3051,11 @@ module.exports = {
             <div class="main_first_img" >
               <img
                 src="https://i.ibb.co/v3ht5Zc/logo.png"
-                style="height: 100px; width: 105px; margin-bottom: -25px"
+                style="height: 100px; width: 105px; margin-top: 60px"
                 alt=""
               />
             </div>
-            <div class="main_first_text" style="margin-top: 60px">
+            <div class="main_first_text" style="margin-top: 200px">
             <div class="main_first_text_tex1">${province.toUpperCase()} - ${geography.toUpperCase()}</div>
               <div class="main_first_text_tex2">INCOME ANALYSIS</div>
             </div>
