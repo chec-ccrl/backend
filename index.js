c7deb7b57467e1ac447ce43a44b8b4ff93bfb80c
shivamks2712 <shivamks2712@gmail.com>
2024-03-27 12:50:45 +0530

    frgf

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ require("dotenv").config();
 app.use(morgan("combined", { stream: logger.stream.write }));
 app.use(express.json());
 app.use(cors());
+app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ extended: true, limit: "500mb" }));
+
 app.use(cookieParser());
 
 const PORT = process.env.SERVER_PORT || 80;
