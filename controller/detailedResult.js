b6758954524c0334ee59ef6c6c1e38ef21eff254
shivamks2712 <shivamks2712@gmail.com>
2024-03-29 21:04:52 +0530

    wdefrg

--- controller/detailedResult.js
+++ controller/detailedResult.js
@@ -1643,7 +1643,9 @@ module.exports = {
           (averageRent / 4) * 12 + cost_of_non_shelter_necessity
         );
       }
-      averageRent = Math.round(averageRent / 10000) * 10000;
+
+      //averageRent = Math.round(averageRent / 10000) * 10000;
+
       let optimal_incomes = [0, 0, 0, 0, 0, 0, 0, 0];
       let optimal_incomes_diff = [0, 0, 0, 0, 0, 0, 0, 0];
       mainmainObj.map((ele) => {
@@ -1851,75 +1853,147 @@ module.exports = {
       canadaIncomeSurveyDetails.map((eke) => {
         if (eke.income_bracket === "$100,000 and over") {
           if (averageRent > 100000) {
-            graph_3_1_color[8] = `"#bb1823"`;
             redPercent += eke.percentage_of_family_total_income;
           } else {
             graph_3_1_color[8] = `"green"`;
           }
+          graph_3_1_color[8] = `"#bb1823"`;
           graph_3_1[8] = eke.percentage_of_family_total_income;
         } else if (eke.income_bracket === "$80,000 to $99,999") {
           if (averageRent > 80000) {
-            graph_3_1_color[7] = `"#bb1823"`;
-            redPercent += eke.percentage_of_family_total_income;
+            if (averageRent < 100000) {
+              redPercent += Number(
+                (
+                  (eke.percentage_of_family_total_income / 19999) *
+                  (99999 - averageRent)
+                ).toFixed(1)
+              );
+            } else {
+              redPercent += eke.percentage_of_family_total_income;
+            }
           } else {
             graph_3_1_color[7] = `"green"`;
           }
+          graph_3_1_color[7] = `"#bb1823"`;
           graph_3_1[7] = eke.percentage_of_family_total_income;
         } else if (eke.income_bracket === "$60,000 to $79,999") {
           if (averageRent > 60000) {
-            graph_3_1_color[6] = `"#bb1823"`;
-            redPercent += eke.percentage_of_family_total_income;
+            if (averageRent < 80000) {
+              redPercent += Number(
+                (
+                  (eke.percentage_of_family_total_income / 19999) *
+                  (79999 - averageRent)
+                ).toFixed(1)
+              );
+            } else {
+              redPercent += eke.percentage_of_family_total_income;
+            }
           } else {
             graph_3_1_color[6] = `"green"`;
           }
+          graph_3_1_color[6] = `"#bb1823"`;
           graph_3_1[6] = eke.percentage_of_family_total_income;
         } else if (eke.income_bracket === "$50,000 to $59,999") {
           if (averageRent > 50000) {
-            graph_3_1_color[5] = `"#bb1823"`;
-            redPercent += eke.percentage_of_family_total_income;
+            if (averageRent < 60000) {
+              redPercent += Number(
+                (
+                  (eke.percentage_of_family_total_income / 9999) *
+                  (59999 - averageRent)
+                ).toFixed(1)
+              );
+            } else {
+              redPercent += eke.percentage_of_family_total_income;
+            }
           } else {
             graph_3_1_color[5] = `"green"`;
           }
+          graph_3_1_color[5] = `"#bb1823"`;
           graph_3_1[5] = eke.percentage_of_family_total_income;
         } else if (eke.income_bracket === "$40,000 to $49,999") {
           if (averageRent > 40000) {
-            graph_3_1_color[4] = `"#bb1823"`;
-            redPercent += eke.percentage_of_family_total_income;
+            if (averageRent < 50000) {
+              redPercent += Number(
+                (
+                  (eke.percentage_of_family_total_income / 9999) *
+                  (49999 - averageRent)
+                ).toFixed(1)
+              );
+            } else {
+              redPercent += eke.percentage_of_family_total_income;
+            }
           } else {
             graph_3_1_color[4] = `"green"`;
           }
+          graph_3_1_color[4] = `"#bb1823"`;
           graph_3_1[4] = eke.percentage_of_family_total_income;
         } else if (eke.income_bracket === "$30,000 to $39,999") {
           if (averageRent > 30000) {
-            graph_3_1_color[3] = `"#bb1823"`;
-            redPercent += eke.percentage_of_family_total_income;
+            if (averageRent < 40000) {
+              redPercent += Number(
+                (
+                  (eke.percentage_of_family_total_income / 9999) *
+                  (39999 - averageRent)
+                ).toFixed(1)
+              );
+            } else {
+              redPercent += eke.percentage_of_family_total_income;
+            }
           } else {
             graph_3_1_color[3] = `"green"`;
           }
+          graph_3_1_color[3] = `"#bb1823"`;
           graph_3_1[3] = eke.percentage_of_family_total_income;
         } else if (eke.income_bracket === "$20,000 to $29,999") {
           if (averageRent > 20000) {
-            graph_3_1_color[2] = `"#bb1823"`;
-            redPercent += eke.percentage_of_family_total_income;
+            if (averageRent < 30000) {
+              redPercent += Number(
+                (
+                  (eke.percentage_of_family_total_income / 9999) *
+                  (29999 - averageRent)
+                ).toFixed(1)
+              );
+            } else {
+              redPercent += eke.percentage_of_family_total_income;
+            }
           } else {
             graph_3_1_color[2] = `"green"`;
           }
+          graph_3_1_color[2] = `"#bb1823"`;
           graph_3_1[2] = eke.percentage_of_family_total_income;
         } else if (eke.income_bracket === "$10,000 to $19,999") {
           if (averageRent > 10000) {
-            graph_3_1_color[1] = `"#bb1823"`;
-            redPercent += eke.percentage_of_family_total_income;
+            if (averageRent < 20000) {
+              redPercent += Number(
+                (
+                  (eke.percentage_of_family_total_income / 9999) *
+                  (19999 - averageRent)
+                ).toFixed(1)
+              );
+            } else {
+              redPercent += eke.percentage_of_family_total_income;
+            }
           } else {
             graph_3_1_color[1] = `"green"`;
           }
+          graph_3_1_color[1] = `"#bb1823"`;
           graph_3_1[1] = eke.percentage_of_family_total_income;
         } else {
           if (averageRent > 0) {
-            graph_3_1_color[0] = `"#bb1823"`;
-            redPercent += eke.percentage_of_family_total_income;
+            if (averageRent < 10000) {
+              redPercent += Number(
+                (
+                  (eke.percentage_of_family_total_income / 9999) *
+                  (9999 - averageRent)
+                ).toFixed(1)
+              );
+            } else {
+              redPercent += eke.percentage_of_family_total_income;
+            }
           } else {
             graph_3_1_color[0] = `"green"`;
           }
+          graph_3_1_color[0] = `"#bb1823"`;
           graph_3_1[0] = eke.percentage_of_family_total_income;
         }
       });
