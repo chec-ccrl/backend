c22e18f44d6718ee954a070dc8d144d8891af259
shivamks2712 <shivamks2712@gmail.com>
2024-03-29 20:35:17 +0530

    sdf

--- services/pdfService.js
+++ services/pdfService.js
@@ -1626,7 +1626,7 @@ module.exports = {
       <html>
         <body>
           <!-- Main ELEMENT FOR COMPLETE PDF -->
-          <div class="main" style="height: 1500px">
+          <div class="main" style="height: 1600px">
             <!-- FIFTH PAGE -->
             <div class="main_first">
               <div class="main_first_img">
@@ -2136,7 +2136,7 @@ module.exports = {
       
           <!-- SIXTH PAGE -->
           <br /><br />
-          <div style="position: relative; height: 1500px">
+          <div style="position: relative; height: 1600px">
             <div class="main_first" >
               <div class="main_first_img">
                 <img
@@ -3054,7 +3054,7 @@ module.exports = {
           </div>
       
           <!-- SEVENTH PAGE -->
-          <div style="height: 1500px;">
+          <div style="height: 1600px;">
           <div class="main_first" >
             <div class="main_first_img" >
               <img
