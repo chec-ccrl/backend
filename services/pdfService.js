1e163600e44b1a0ce29c80f072f11131edb184d9
shivamks2712 <shivamks2712@gmail.com>
2024-04-01 12:36:29 +0530

    ergt

--- services/pdfService.js
+++ services/pdfService.js
@@ -3105,7 +3105,7 @@ module.exports = {
               PERCENTAGE OF POPULATION BELOW OPTIMAL INCOME <br />
               ${redPercent.toFixed(1)}%
               <span style="position: absolute; left: -4px; bottom: -5.5px"> < </span>
-              <span style="position: absolute; right: -4px; bottom: -5.5px"> > </span>
+              <span style="position: absolute; right: -4px; bottom: -5px"> > </span>
             </p>
             <p
               style="
