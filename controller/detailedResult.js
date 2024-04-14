8397eee1f3c1e17623ae00b2691d6bf3778bdcba
shivamks2712 <shivamks2712@gmail.com>
2024-04-14 15:56:37 +0530

    ff

--- controller/detailedResult.js
+++ controller/detailedResult.js
@@ -2912,14 +2912,14 @@ module.exports = {
         geography === "Grande Prairie"
       ) {
         tableHeigth = 2.3;
-      } else if (geography === "Victoria") {
+      } else if (geography === "Victoria" || geography === "Red Deer") {
         tableHeigth = 3.2;
         invertHiegt = 300;
         height = 1750;
       } else if (geography === "Vancouver" || geography === "Medicine Hat") {
         tableHeigth = 2.8;
         height = 1700;
-      } else if (geography === "Edmonton" || geography === "Red Deer") {
+      } else if (geography === "Edmonton") {
         tableHeigth = 3.5;
         invertHiegt = 400;
         height = 1900;
