a39dd1a04e2c019885402f2c2dba27066202b2a7
shivamks2712 <shivamks2712@gmail.com>
2024-04-14 15:11:15 +0530

    ferfer

--- controller/detailedResult.js
+++ controller/detailedResult.js
@@ -2906,7 +2906,7 @@ module.exports = {
       if (geography === "Penticton") {
         tableHeigth = 2.3;
       } else if (geography === "Victoria") {
-        tableHeigth = 2.7;
+        tableHeigth = 3;
       }
       // if (
       //   geography === "Gander" ||
