cf08133ec061157c3285e107e7f3521a9f0cd1dd
shivamks2712 <shivamks2712@gmail.com>
2024-04-14 17:18:54 +0530

    f

--- services/pdfService.js
+++ services/pdfService.js
@@ -2135,7 +2135,7 @@ module.exports = {
       
           <!-- SIXTH PAGE -->
           <br /><br />
-          <div style="position: relative; height: ${height}px;">
+          <div style="position: relative; height: ${height - 100}px;">
             <div class="main_first" >
               <div class="main_first_img">
                 <img
@@ -3055,7 +3055,7 @@ module.exports = {
           </div>
       
           <!-- SEVENTH PAGE -->
-          <div style="height: ${height}px;">
+          <div style="height: ${height - 50}px;">
           <div class="main_first" >
             <div class="main_first_img" >
               <img
