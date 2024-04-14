fcc5f9fcfb754119cdebef244bb56623284cb737
shivamks2712 <shivamks2712@gmail.com>
2024-04-14 15:35:14 +0530

    f

--- controller/detailedResult.js
+++ controller/detailedResult.js
@@ -2919,9 +2919,9 @@ module.exports = {
         tableHeigth = 2.8;
         height = 1700;
       } else if (geography === "Edmonton" || geography === "Calgary") {
-        tableHeigth = 3;
+        tableHeigth = 3.2;
         invertHiegt = 350;
-        height = 1800;
+        height = 1850;
       }
       // if (
       //   geography === "Gander" ||
