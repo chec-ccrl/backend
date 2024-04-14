aaed66b86c68325f5d1573dc4fc24e88883df819
shivamks2712 <shivamks2712@gmail.com>
2024-04-14 14:59:34 +0530

    kdvnvkevkemvl;e,v;,flverlkve,rv

--- controller/detailedResult.js
+++ controller/detailedResult.js
@@ -2901,7 +2901,7 @@ module.exports = {
       final_rank = findRank(cma === "Na" ? ca : cma, rankingArr);
 
       let tableHeigth = 2;
-      let height = 1650;
+      let height = 1660;
       let invertHiegt = 250;
       // if (
       //   geography === "Gander" ||
