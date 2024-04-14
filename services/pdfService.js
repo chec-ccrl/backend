b2263e99cda81c3c8594e3344a85b40a7c878e68
shivamks2712 <shivamks2712@gmail.com>
2024-04-14 14:06:17 +0530

    dfg

--- services/pdfService.js
+++ services/pdfService.js
@@ -1899,7 +1899,7 @@ module.exports = {
               ROW HOUSE & APARTMENT
             </p>
       
-            <div class="flex justify-between" style="min-height:330px;max-height:331px">
+            <div class="flex justify-between" style="min-height:380px;max-height:381px">
               <div class="main_page4_first_one_one_main">
                 <div class="main_page3_first_one_sub">
                   <div class="main_page3_first_one_txt" style="letter-spacing: 6px">
