023fa109dafb3597eb5c8e4889d2d576519fd8af
shivamks2712 <shivamks2712@gmail.com>
2024-04-28 18:12:19 +0530

    erf

--- services/pdfService.js
+++ services/pdfService.js
@@ -1623,7 +1623,7 @@ module.exports = {
       <html>
         <body>
           <!-- Main ELEMENT FOR COMPLETE PDF -->
-          <div class="main" style="min-height:1600px" >
+          <div class="main" style="min-height:1560px" >
             <!-- FIFTH PAGE -->
             <div class="main_first">
               <div class="main_first_img">
