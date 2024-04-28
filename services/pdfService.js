d934c5686850f46da7bb90ff27a39742dd3edfab
shivamks2712 <shivamks2712@gmail.com>
2024-04-28 17:20:20 +0530

    lk

--- services/pdfService.js
+++ services/pdfService.js
@@ -3233,6 +3233,31 @@ module.exports = {
               <span style="margin-left: 50px;margin-top:20px"> INCOME IS TAKEN ANUALLY </span>
               <div class="main_third_line"></div>
             </div>
+            <div class="main_page6_first_one">
+              <div class="main_page6_first">
+                <div class="main_page6_first_one_charts">
+                  <div class="main_page6_first_one_charts_one">
+                    <div class="main_page4_first_one_one_head text-xl ml-3">
+                      APARTMENT OVERVIEW
+                    </div>
+                    <canvas
+                    id="main_page6_first_one_charts_one_21"
+                    style="width: 550px"
+                    ></canvas>
+                  </div>
+                  <div class="main_page3_first_one_break"></div>
+                  <div class="main_page6_first_one_charts_two">
+                    <div class="main_page4_first_one_one_head text-xl mr-3">
+                      ROW HOUSE OVERVIEW
+                    </div>
+                    <canvas
+                      id="main_page6_first_one_charts_two_22"
+                      style="width: 550px"
+                    ></canvas>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </body>
         <script>
