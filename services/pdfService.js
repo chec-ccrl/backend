7dcec834f7c73fec31e3fd4065f355497ea0ec01
shivamks2712 <shivamks2712@gmail.com>
2024-04-28 17:14:03 +0530

    efef

--- services/pdfService.js
+++ services/pdfService.js
@@ -3299,6 +3299,61 @@ module.exports = {
               </div>
               <div class="main_third_line"></div>
             </div>
+            <div class="main_page6_first_one">
+              <div class="main_page6_first_one_flex">
+                <div>
+                  <div class="main_page6_first_one_head text-lg">
+                    TOP 5 CMAs - <br />
+                    AVERAGE RENT
+                  </div>
+                  <div class="main_page6_first_one_charts_three m-auto">
+                    <canvas
+                      id="main_page6_first_one_charts_one_27"
+                      style="height: 220px; max-width: 270px"
+                    ></canvas>
+                  </div>
+                </div>
+                <div class="main_page3_first_one_break"></div>
+                <div>
+                  <div class="main_page6_first_one_head text-lg">
+                    BOTTOM 5 CMAs - <br />
+                    AVERAGE RENT
+                  </div>
+                  <div class="main_page6_first_one_charts_three m-auto">
+                    <canvas
+                      id="main_page6_first_one_charts_two_28"
+                      style="height: 220px; max-width: 270px"
+                    ></canvas>
+                  </div>
+                </div>
+                <div class="main_page3_first_one_break"></div>
+                <div>
+                  <div class="main_page6_first_one_head text-lg">
+                    TOP 5 CAs - <br />
+                    AVERAGE RENT
+                  </div>
+                  <div class="main_page6_first_one_charts_three m-auto">
+                    <canvas
+                      id="main_page6_first_one_charts_one_35"
+                      style="height: 220px; max-width: 270px"
+                    ></canvas>
+                  </div>
+                </div>
+                <div class="main_page3_first_one_break"></div>
+                <div>
+                  <div class="main_page6_first_one_head text-lg">
+                    BOTTOM 5 CAs - <br />
+                    AVERAGE RENT
+                  </div>
+                  <div class="main_page6_first_one_charts_three m-auto">
+                    <canvas
+                      id="main_page6_first_one_charts_two_36"
+                      style="height: 220px; max-width: 270px"
+                    ></canvas>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </body>
         <script>
