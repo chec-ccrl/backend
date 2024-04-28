2b7acc48b9b9d629262091d1aedc33cda14a469d
shivamks2712 <shivamks2712@gmail.com>
2024-04-28 18:16:44 +0530

    p

--- services/pdfService.js
+++ services/pdfService.js
@@ -3249,7 +3249,7 @@ module.exports = {
                 </div>
               </div>
             </div>
-            <div class="main_page6_first_one_head ls-2 py-1 m-0" style="margin-left:400px">
+            <div class="main_page6_first_one_head mb-2" style="margin-left:400px">
              AVERAGE RENT
             </div>
             <div class="main_page6_first_one">
@@ -3266,7 +3266,7 @@ module.exports = {
               </div>
             </div>
             <div class="main_third_line"></div>
-            <div class="main_page6_first_one_head ls-2 py-1 m-0" style="margin-left:220px">
+            <div class="main_page6_first_one_head mb-2" style="margin-left:220px">
               HISTORICAL RENT GROWTH RATE
             </div>
             <div class="main_page6_first_one">
