92553da80f95b1f554bb1f2953bd6081762fda3f
shivamks2712 <shivamks2712@gmail.com>
2024-04-28 13:01:15 +0530

    .

--- controller/detailedResult.js
+++ controller/detailedResult.js
@@ -2923,7 +2923,7 @@ module.exports = {
       final_rank = findRank(cma === "Na" ? ca : cma, rankingArr);
 
       let tableHeigth = 2.2;
-      let height = 1760;
+      let height = 1500;
       let invertHiegt = 250;
       // if (
       //   geography === "Penticton" ||
